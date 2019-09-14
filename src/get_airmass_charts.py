e331747410433a37801e2fc49eb6db19625a1e93
lgbouma <bouma.luke@gmail.com>
2019-09-14 15:03:51 -0400

    get airmass charts: now works
    though it plots observable fraction per day, rather than airmass. (more
    in line with what we want)

--- src/get_airmass_charts.py
+++ src/get_airmass_charts.py
@@ -6,36 +6,29 @@ from astropy.coordinates import SkyCoord
 import astropy.units as u
 from astropy.time import Time
 
-from astroplan.plots import plot_airmass
-from astroplan import (FixedTarget, Observer, months_observable,
-                       AtNightConstraint, AltitudeConstraint)
-
-import datetime as dt
+from astroplan import (
+    FixedTarget, Observer, months_observable, AtNightConstraint,
+    AltitudeConstraint, is_observable, observability_table,
+    AirmassConstraint
+)
 
 def make_airmass_chart(name="WASP 4", site='keck', ra=None, dec=None,
                        start_time=Time('2019-09-13 20:00:00'),
                        end_time=Time('2020-07-31 20:00:00'),
-                       outdir='../results/followup_planning/WASP_4'):
+                       outdir='../results/followup_planning/WASP_4',
+                       check_months_observable=True):
     """
     2020A:
         - Feb 1 - Jul 31 2020
     """
 
-    if (
-        not (isinstance(name,str)
-             and ra is None
-             and dec is None)
-        and not (isinstance(name,None)
-             and isinstance(ra,float)
-             and isinstance(dec,float))
-    ):
-        raise NotImplementedError('Either give name, or give ra and dec.')
-
-    if isinstance(name, str):
+    if isinstance(name,str) and ra is None and dec is None:
         target = FixedTarget.from_name(name)
     elif isinstance(ra,float) and isinstance(dec,float):
-        #FIXME
-        target = FixedTarget.from_name(name)
+        target_coord = SkyCoord(ra=ra*u.deg, dec=dec*u.deg)
+        target = FixedTarget(coord=target_coord, name=name)
+    else:
+        raise NotImplementedError('failed to make target')
 
     if not os.path.exists(outdir):
         os.mkdir(outdir)
@@ -43,39 +36,77 @@ def make_airmass_chart(name="WASP 4", site='keck', ra=None, dec=None,
     observer = Observer.at_site(site)
 
     constraints = [AltitudeConstraint(min=20*u.deg, max=85*u.deg),
+                   AirmassConstraint(3),
                    AtNightConstraint.twilight_civil()]
 
-    observe_time = Time(
+    # over every day between start and end time, check if the observing
+    # constraints are meetable.
+    days = Time(
         np.arange(start_time.decimalyear, end_time.decimalyear,
-                  0.5/(24*365.25)),
+                  1/(365.25)),
         format='decimalyear'
     )
 
-    #FIXME this isn't working ?!
-    import IPython; IPython.embed()
+    frac, ever_observable = [], []
 
-    # FIXME want this?
-    # computed observability on "best_months" grid of 0.5 hr
-    # best_months = months_observable(constraints, observer, [target])
-    # print('for {}, got best-months on 0.5 hour grid:'.format(name))
-    # print(best_months)
-    # print('where 1 = Jan, 2 = Feb, etc.')
+    for day in days:
 
+        table = observability_table(constraints, observer, [target],
+                                    time_range=day)
+        frac.append(float(table['fraction of time observable']))
+        ever_observable.append(bool(table['ever observable']))
+
+    ever_observable = np.array(ever_observable)
+    frac = np.array(frac)
+
+    #
+    # make the plot
+    #
     plt.close('all')
+    fig, ax = plt.subplots(figsize=(4,3))
 
-    plot_airmass(target, observer, observe_time)
+    ax.plot_date(days.plot_date, frac, label=name, lw=1,
+                 alpha=1, fmt='k-')
 
-    outpath = os.path.join(outdir, 'airmass.png')
-    plt.savefig(outpath, dpi=300)
+    ax.set_xlim([days[0].plot_date, days[-1].plot_date])
+    ax.set_title(
+        '{}: altitude 20-85deg, airmass<3, w/in civil twilight'.format(name),
+        fontsize='small'
+    )
+    plt.setp(ax.get_xticklabels(), rotation=90, ha='right')
+
+    ax.set_xlabel('time')
+    ax.set_ylabel('fraction visible')
+
+    outpath = os.path.join(
+        outdir,
+        'frac_visible_{:s}.png'.format(site.replace(' ','_').lower())
+    )
+    fig.savefig(outpath, dpi=300, bbox_inches='tight')
     print('saved {}'.format(outpath))
 
+    #
+    # optionally computes observability on "best_months" grid of 0.5 hr
+    #
+    if check_months_observable:
+        best_months = months_observable(constraints, observer, [target])
+        outfile = os.path.join(outdir,'months_observable.txt')
+        with open(outfile, 'w') as f:
+            f.write('for {}, got best-months on 0.5 hour grid:'.format(name))
+            f.write(repr(best_months))
+            f.write('where 1 = Jan, 2 = Feb, 3=March, etc.')
+        print('made {}'.format(outfile))
 
 
 if __name__ == "__main__":
 
     name='WASP 4'
 
-    make_airmass_chart(name=name, outdir='../results/followup_planning/WASP_4',
-                       start_time=Time('2018-09-13 20:00:00', format='iso'),
-                       end_time=Time('2021-09-13 20:00:00', format='iso')
-                      )
+    make_airmass_chart(
+        name=name, site='keck',
+        ra=None, dec=None,
+        outdir='../results/followup_planning/WASP_4',
+        start_time=Time('2019-09-13 20:00:00', format='iso'),
+        end_time=Time('2020-09-13 20:00:00', format='iso'),
+        check_months_observable=False
+    )
