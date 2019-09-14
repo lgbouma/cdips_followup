8f720bc745bcaaca0ec4be242b7c9561610e8d46
lgbouma <bouma.luke@gmail.com>
2019-09-14 15:49:16 -0400

    add multisite plot option
    helps answer the question: where is it best to observe this object from?
    (for arbitrary sites...)

--- src/get_observability_charts.py
+++ src/get_observability_charts.py
@@ -12,15 +12,10 @@ from astroplan import (
     AirmassConstraint
 )
 
-def make_observability_chart(name="WASP 4", site='keck', ra=None, dec=None,
-                             start_time=Time('2019-09-13 20:00:00'),
-                             end_time=Time('2020-07-31 20:00:00'),
-                             outdir='../results/followup_planning/WASP_4',
-                             check_months_observable=True):
-    """
-    2020A:
-        - Feb 1 - Jul 31 2020
-    """
+
+def get_observability_fraction(name="WASP 4", site='keck', ra=None, dec=None,
+                               start_time=Time('2019-09-13 20:00:00'),
+                               end_time=Time('2020-07-31 20:00:00')):
 
     if isinstance(name,str) and ra is None and dec is None:
         target = FixedTarget.from_name(name)
@@ -29,9 +24,6 @@ def make_observability_chart(name="WASP 4", site='keck', ra=None, dec=None,
         target = FixedTarget(coord=target_coord, name=name)
     else:
         raise NotImplementedError('failed to make target')
-
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
 
     observer = Observer.at_site(site)
 
@@ -59,6 +51,21 @@ def make_observability_chart(name="WASP 4", site='keck', ra=None, dec=None,
     ever_observable = np.array(ever_observable)
     frac = np.array(frac)
 
+    return frac, ever_observable, days
+
+
+
+def make_observability_chart_singlesite(
+    name="WASP 4", site='keck', ra=None, dec=None,
+    start_time=Time('2019-09-13 20:00:00'),
+    end_time=Time('2020-07-31 20:00:00'),
+    outdir=None):
+
+    frac, ever_observable, days = get_observability_fraction(
+        name=name, site=site, ra=ra, dec=dec,
+        start_time=start_time, end_time=end_time
+    )
+
     #
     # make the plot
     #
@@ -71,7 +78,7 @@ def make_observability_chart(name="WASP 4", site='keck', ra=None, dec=None,
     ax.set_xlim([days[0].plot_date, days[-1].plot_date])
     ax.set_title(
         '{}: altitude 20-85deg, airmass<3, w/in civil twilight'.format(name),
-        fontsize='small'
+        fontsize='xx-small'
     )
     plt.setp(ax.get_xticklabels(), rotation=90, ha='right')
 
@@ -86,17 +93,52 @@ def make_observability_chart(name="WASP 4", site='keck', ra=None, dec=None,
     fig.savefig(outpath, dpi=300, bbox_inches='tight')
     print('saved {}'.format(outpath))
 
+
+def make_observability_chart_multisite(
+    name="WASP 4", sites=['keck','las campanas'], ra=None, dec=None,
+    start_time=Time('2019-09-13 20:00:00'),
+    end_time=Time('2020-07-31 20:00:00'),
+    outdir=None):
+
+    fracs = []
+    for site in sites:
+        frac, _, days = get_observability_fraction(
+            name=name, site=site, ra=ra, dec=dec,
+            start_time=start_time, end_time=end_time
+        )
+        fracs.append(frac)
+
     #
-    # optionally computes observability on "best_months" grid of 0.5 hr
+    # make the plot
     #
-    if check_months_observable:
-        best_months = months_observable(constraints, observer, [target])
-        outfile = os.path.join(outdir,'months_observable.txt')
-        with open(outfile, 'w') as f:
-            f.write('for {}, got best-months on 0.5 hour grid:'.format(name))
-            f.write(repr(best_months))
-            f.write('where 1 = Jan, 2 = Feb, 3=March, etc.')
-        print('made {}'.format(outfile))
+    plt.close('all')
+    fig, ax = plt.subplots(figsize=(4,3))
+
+    for ix, frac in enumerate(fracs):
+        ax.plot_date(days.plot_date, frac*24, lw=1, alpha=1,
+                     fmt='C{}-'.format(ix), label=sites[ix])
+
+    ax.set_xlim([days[0].plot_date, days[-1].plot_date])
+    ax.set_title(
+        '{}: altitude 20-85deg, airmass<3, w/in civil twilight'.format(name),
+        fontsize='xx-small'
+    )
+    plt.setp(ax.get_xticklabels(), rotation=75, ha='right', fontsize='small')
+    plt.setp(ax.get_yticklabels(), fontsize='small')
+
+    ax.legend(loc='upper right', fontsize='xx-small')
+
+    ax.set_xlabel('time')
+    ax.set_ylabel('hours visible per night')
+    ax.set_ylim((0,12))
+
+    outpath = os.path.join(
+        outdir,
+        'frac_visible_multisite.png'
+    )
+    fig.tight_layout()
+    fig.savefig(outpath, dpi=300, bbox_inches='tight')
+    print('saved {}'.format(outpath))
 
 
 if __name__ == "__main__":
@@ -108,6 +150,5 @@ if __name__ == "__main__":
         ra=None, dec=None,
         outdir='../results/followup_planning/WASP_4',
         start_time=Time('2019-09-13 20:00:00', format='iso'),
-        end_time=Time('2020-09-13 20:00:00', format='iso'),
-        check_months_observable=False
+        end_time=Time('2020-09-13 20:00:00', format='iso')
     )
--- src/run_2020A_youngstar_observabilities.py
+++ src/run_2020A_youngstar_observabilities.py
@@ -3,11 +3,13 @@ from numpy import array as nparr
 import os
 from glob import glob
 
-from get_observability_charts import make_observability_chart
-
+from get_observability_charts import (
+    make_observability_chart_singlesite,
+    make_observability_chart_multisite
+)
 from astropy.time import Time
 
-def main(site='keck'):
+def single_site(site='keck'):
 
     df = pd.read_csv('../data/20190912_youngstar_cands_with_gaia.csv')
 
@@ -19,17 +21,46 @@ def main(site='keck'):
         if not os.path.exists(outdir):
             os.mkdir(outdir)
 
-        make_airmass_chart(
+        # 2020A: Feb 1 - Jul 31 2020
+        make_observability_chart_singlesite(
             name=name, site=site,
             ra=ra, dec=dec,
             outdir=outdir,
             start_time=Time('2019-09-13 20:00:00', format='iso'),
-            end_time=Time('2020-09-13 20:00:00', format='iso'),
-            check_months_observable=False
+            end_time=Time('2020-09-13 20:00:00', format='iso')
         )
 
+def multi_site(
+    sites=['keck',
+           'Las Campanas Observatory',
+           'Kitt Peak National Observatory']
+):
+
+    df = pd.read_csv('../data/20190912_youngstar_cands_with_gaia.csv')
+
+    for name, ra, dec in zip(
+        nparr(df['source_id']), nparr(df['ra']), nparr(df['dec'])
+    ):
+
+        outdir = '../results/followup_planning/{}'.format(name)
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+
+        # 2020A: Feb 1 - Jul 31 2020
+        make_observability_chart_multisite(
+            name=name, sites=sites,
+            ra=ra, dec=dec,
+            outdir=outdir,
+            start_time=Time('2019-09-13 20:00:00', format='iso'),
+            end_time=Time('2020-09-13 20:00:00', format='iso')
+        )
+
+
 if __name__ == "__main__":
-    main(site='Las Campanas Observatory') # PFS, PISCO
-    main(site='Kitt Peak National Observatory') # TRES(?), NEID
-    main(site='keck') # HIRES, Subaru
-    # main(site='lapalma') # HARPS-N
+
+    multi_site()
+
+    # single_site(site='Las Campanas Observatory') # PFS, PISCO
+    # single_site(site='Kitt Peak National Observatory') # TRES(?), NEID
+    # single_site(site='keck') # HIRES, Subaru
+    # single_site(site='lapalma') # HARPS-N
