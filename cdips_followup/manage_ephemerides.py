c96004e9ece09c17576cb9645c604a9abf8a3633
lgbouma <bouma.luke@gmail.com>
2019-12-07 12:20:41 -0500

    add ephemeris management

--- cdips_followup/manage_ephemerides.py
+++ cdips_followup/manage_ephemerides.py
@@ -19,10 +19,15 @@ Tools:
 # imports and config #
 ######################
 
-import pandas as pd
-import socket
+import pandas as pd, numpy as np
+import socket, os
 from datetime import datetime
 from parse import search
+
+from astrobase.services.identifiers import tic_to_gaiadr2
+
+from cdips.utils import today_YYYYMMDD
+from cdips.utils.catalogs import get_exofop_toi_catalog
 
 if socket.gethostname() == 'ast1607-astro':
     EPHEM_PATH = '/Users/luke/Dropbox/proj/cdips_followup/data/ephemerides/ephemerides.csv'
@@ -37,15 +42,21 @@ else:
 # general-purpose functions #
 #############################
 
-def insert_ephemeris(ephemsourcefile, ephemeris_type=None):
+def insert_ephemeris(ephemeris_type=None, targetid=None, ephemsourcefile=None):
     """
-    ephemsourcefile: path to source file containing the ephemeris information.
-
     ephemeris_type: type of ephemeris source file. These could be Hartman's
-    text files, cdips-pipeline's pickles, ExoFOP-TESS CSV files, or similar.
+    text files, cdips-pipeline, ExoFOP-TESS CSV files, or MIT QLP toi-plus
+    catalog:  ['cdipspipeline', 'exofoptess', 'hartmanupdate', 'toiplus']
+
+    targetid: if type is MITQLP toi-plus, then e.g., 'TIC308538095.01' or
+    '451.01'.
+
+    ephemsourcefile: if type is Hartman's text files, then path to source file
+    containing the ephemeris information.
     """
 
-    valid_types = ['cdips-pipeline', 'ExoFOP-TESS', 'hartman_updatephem']
+    valid_types = ['cdipspipeline', 'exofoptess', 'hartmanupdate',
+                   'toiplus']
 
     if ephemeris_type not in valid_types:
         errmsg = (
@@ -53,27 +64,80 @@ def insert_ephemeris(ephemsourcefile, ephemeris_type=None):
             format(ephemeris_type)
         )
         raise ValueError(errmsg)
+    if ephemeris_type =='hartmanupdate':
+        assert isinstance(ephemsourcefile, str)
+    if ephemeris_type =='toiplus':
+        assert isinstance(targetid, str)
 
     #
     # Retrieve dict with period, epoch, duration (and uncertainties).
     #
-    if ephemeris_type == 'hartman_updatephem':
+    if ephemeris_type == 'hartmanupdate':
         ephem_dict = read_hartman_updateephem_file(ephemsourcefile)
 
-    elif ephemeris_type == 'cdips-pipeline':
-        raise NotImplementedError
+    elif ephemeris_type == 'exofoptess':
+        wrnmsg = (
+            'WRN! GETTING EPHEMERIS FROM EXOFOPTESS CATALOG. ENSURE LATEST '
+            'VERSION HAS BEEN DOWNLOADED'
+        )
+        print(wrnmsg)
+        ephem_dict = read_exofoptess_ephem(targetid)
 
-    elif ephemeris_type == 'ExoFOP-TESS':
+    else:
         raise NotImplementedError
 
     #
     # Construct and insert the new row.
     #
+
+    if ephemeris_type == 'hartmanupdate':
+        if 'TIC' not in ephemsourcefile:
+            errmsg = (
+                'assumed {} had TIC*.updatepehem.txt'.
+                format(ephemsourcefile)
+            )
+            raise ValueError(errmsg)
+        # assumes ../data/updated_ephemerides/20191030/TIC308538095.updateephem.txt
+        ticid = os.path.basename(ephemsourcefile).split('.')[0].lstrip('TIC')
+        source_id = tic_to_gaiadr2(ticid)
+        targetid = 'TIC{}.01'.format(ticid)
+        ephemeris_origin = os.path.abspath(ephemsourcefile)
+
+    elif ephemeris_type == 'exofoptess':
+
+        toidf = get_exofop_toi_catalog()
+        sel = toidf['TOI'].astype(str) == targetid
+        targetrow = toidf[sel]
+
+        ticid = str(targetrow['TIC ID'].iloc[0])
+        source_id = tic_to_gaiadr2(ticid)
+        targetid = targetid
+        ephemeris_origin = get_exofop_toi_catalog(returnpath=True)
+
+    else:
+        raise NotImplementedError
+
+    new_row = pd.DataFrame({
+        'source_id': source_id,
+        'ticid': ticid,
+        'targetid': targetid,
+        'insert_time': pd.Timestamp.now(),
+        'period': ephem_dict['period_val'],
+        'period_unc': ephem_dict['period_unc'],
+        'epoch': ephem_dict['epoch_val'],
+        'epoch_unc': ephem_dict['epoch_unc'],
+        'duration': ephem_dict['duration_val'],
+        'duration_unc': ephem_dict['duration_unc'],
+        'depth': ephem_dict['depth_val'],
+        'depth_unc': ephem_dict['depth_unc'],
+        'ephemeris_origin': ephemeris_origin
+    }, index=[0])
+
     ephem_df = pd.read_csv(EPHEM_PATH)
 
-    import IPython; IPython.embed()
-    assert 0
-    save_ephemerides_csv_file(ephem_df)
+    new_ephem_df = pd.concat((ephem_df, new_row), sort=False)
+
+    save_ephemerides_csv_file(new_ephem_df)
 
 
 def get_ephemeris(source_id=None, ticid=None):
@@ -94,8 +158,33 @@ def get_ephemeris(source_id=None, ticid=None):
 
 
 def save_ephemerides_csv_file(ephem_df):
+    """
+    Save function does two things:
+        1. If ephemerides_{YYYYMMDD}.csv doesn't exist, it will make it. Even
+        though ephemerides.csv is version-controlled, this is a simple
+        mechanism to allow periodic backups, rate-limited at one day.
+        2. Overwrite ephemerides.csv
+    """
 
-    ephem_df.sort_values(by='insert_time').to_csv(
+    EPHEM_DAY_PATH = os.path.join(
+        os.path.dirname(EPHEM_PATH),
+        'ephemerides_{}.csv'.format(today_YYYYMMDD())
+    )
+    colorder = ['source_id', 'ticid', 'targetid', 'insert_time', 'period',
+                'period_unc', 'epoch', 'epoch_unc', 'depth', 'depth_unc',
+                'duration', 'duration_unc', 'ephemeris_origin']
+
+    ephem_df['insert_time'] = pd.to_datetime(ephem_df.insert_time)
+    out_df = ephem_df[colorder].sort_values(by='insert_time')
+
+    if not os.path.exists(EPHEM_DAY_PATH):
+        out_df.to_csv(
+            EPHEM_DAY_PATH, index=False, float_format='%.8f'
+        )
+        print('{}: backed up to {}'.
+              format(datetime.utcnow(), EPHEM_DAY_PATH))
+
+    out_df.to_csv(
         EPHEM_PATH, index=False, float_format='%.8f'
     )
     print('{}: overwrite {} with updated values'.
@@ -115,24 +204,123 @@ def read_hartman_updateephem_file(updateephempath, verbose=0):
     epoch = [l for l in lines if '- Epoch: ' in l][0]
     period = [l for l in lines if '- Period: ' in l][0]
     dur = [l for l in lines if '- Transit duration: ' in l][0]
+    rprs = [l for l in lines if '- Rp/R*: ' in l][0]
 
     if verbose:
         print(epoch, period, dur)
 
-    epoch = float(search('{} - Epoch: {} +/- {}', epoch)[1].strip())
-    period = float(search('{} - Period: {} +/- {}', period)[1].strip())
-    dur = float(search('{} - Transit duration: {} +/- {}', dur)[1].strip())
+    epoch_val = float(search('{} - Epoch: {} +/- {} (20', epoch)[1].strip())
+    epoch_unc = float(search('{} - Epoch: {} +/- {} (20', epoch)[2].strip())
 
-    #TODO get unc
-    import IPython; IPython.embed()
+    period_val = float(search('{} - Period: {} +/- {}\n', period)[1].strip())
+    period_unc = float(search('{} - Period: {} +/- {}\n', period)[2].strip())
+
+    dur_val = float(search('{} - Transit duration: {} +/- {} hours', dur)[1].strip())
+    dur_unc = float(search('{} - Transit duration: {} +/- {} hours', dur)[2].strip())
+
+    #  - Rp/R*:  0.18033 +-  0.00274
+    rprs_val = float(search('{} - Rp/R*: {} +- {}\n', rprs)[1].strip())
+    rprs_unc = float(search('{} - Rp/R*: {} +- {}\n', rprs)[2].strip())
+
+    depth_val = rprs_val**2
+    depth_unc = np.abs(
+        2 * rprs_unc / rprs_val
+    )
 
     if verbose:
-        print(epoch, period, dur)
+        print(epoch_val, epoch_unc, period_val, period_unc, dur_val, dur_unc)
 
     ephem_dict = {
-        'period':period,
-        'epoch':epoch,
-        'duration':dur
+        'period_val':period_val,
+        'epoch_val':epoch_val,
+        'duration_val':dur_val,
+        'period_unc':period_unc,
+        'epoch_unc':epoch_unc,
+        'duration_unc':dur_unc,
+        'depth_val':depth_val*1e6,
+        'depth_unc':depth_unc*1e6
     }
+
+    return ephem_dict
+
+
+def read_exofoptess_ephem(targetid):
+
+    toidf = get_exofop_toi_catalog()
+
+    if targetid.startswith('TIC'):
+        raise NotImplementedError
+
+    else:
+        # targetid is Full TOI ID
+        sel = toidf['TOI'].astype(str) == targetid
+        targetrow = toidf[sel]
+
+        if not len(targetrow) == 1:
+            raise AssertionError(
+                'failed to get ID match for {}'.format(targetid)
+            )
+
+    keymatchdict = {
+        'period_val': 'Period (days)',
+        'epoch_val': 'Epoch (BJD)',
+        'duration_val': 'Duration (hours)',
+        'period_unc': 'Period (days) err',
+        'epoch_unc': 'Epoch (BJD) err',
+        'duration_unc': 'Duration (hours) err',
+        'depth_val': 'Depth (ppm)',
+        'depth_unc': 'Depth (ppm) err'
+    }
+
+    ephem_dict  = {}
+    for k,v in keymatchdict.items():
+        result = np.float64(targetrow[v])
+        ephem_dict[k] = result
+
+    return ephem_dict
+
+
+def read_mitqlp_ephem(targetid):
+    # NOTE: WIP
+    raise NotImplementedError
+
+    toidf = get_exofop_toi_catalog()
+
+    if targetid.startswith('TIC'):
+        raise NotImplementedError
+
+    else:
+        # targetid is Full TOI ID
+        sel = toidf['Full TOI ID'].astype(str) == targetid
+        targetrow = toidf[sel]
+
+        if not len(targetrow) == 1:
+            raise AssertionError(
+                'failed to get ID match for {}'.format(targetid)
+            )
+
+    keymatchdict = {
+        'period_val': 'Orbital Period Value',
+        'epoch_val': 'Orbital Epoch Value',
+        'duration_val': 'Transit Duration Value',
+        'period_unc': 'Orbital Period Error',
+        'epoch_unc': 'Orbital Epoch Error',
+        'duration_unc': 'Transit Duration Error',
+        'depth_val': 'Transit Depth Value',
+        'depth_unc': 'Transit Depth Error'
+    }
+
+    ephem_dict  = {}
+    for k,v in keymatchdict.items():
+
+        if k == 'epoch_val':
+            # BTJD := BJD - 2457000
+            result = 2457000 + np.float64(targetrow[v])
+        else:
+            result = np.float64(targetrow[v])
+
+        ephem_dict[k] = result
+
+    import IPython; IPython.embed()
 
     return ephem_dict
