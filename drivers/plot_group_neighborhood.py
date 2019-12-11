9e0f67431a4b382bc4af52e85064496525245b15
lgbouma <bouma.luke@gmail.com>
2019-12-11 14:28:08 -0500

    mv bulk neighborhood queries to cdips.utils
    see
    
    https://github.com/lgbouma/cdips/commit/7aceab8fe6eb716e3e7786e549bf94a55820f8c6

--- drivers/plot_group_neighborhood.py
+++ drivers/plot_group_neighborhood.py
@@ -24,6 +24,12 @@ from astroquery.gaia import Gaia
 
 from astrobase.services.gaia import objectid_search
 
+from cdips.utils.gaiaqueries import (
+    make_votable_given_source_ids,
+    given_votable_get_df,
+    given_source_ids_get_gaia_data
+)
+
 datadir = '../data/'
 
 homedir='/home/luke/'
@@ -41,111 +47,6 @@ if not os.path.exists(gaiadir):
 #############
 # functions #
 #############
-def make_votable_given_source_ids(source_ids, outpath=None):
-
-    t = Table()
-    t['source_id'] = source_ids
-
-    votable = from_table(t)
-
-    writeto(votable, outpath)
-    print('made {}'.format(outpath))
-
-    return outpath
-
-
-def given_votable_get_df(votablepath, assert_equal='source_id'):
-
-    vot = parse(votablepath)
-    tab = vot.get_first_table().to_table()
-    df = tab.to_pandas()
-
-    if isinstance(assert_equal, str):
-        np.testing.assert_array_equal(tab[assert_equal], df[assert_equal])
-
-    return df
-
-def given_source_ids_get_gaia_data(source_ids, groupname, overwrite=True,
-                                   enforce_all_sourceids_viable=True):
-    """
-    Args:
-
-        source_ids (np.ndarray) of np.int64 Gaia DR2 source_ids
-
-        groupname (str)
-
-        overwrite: if True, and finds that this crossmatch has already run,
-        deletes previous cached output and reruns anyway.
-
-        enforce_all_sourceids_viable: if True, will raise an assertion error if
-        every source id does not return a result.
-
-    Returns:
-
-        dataframe with Gaia DR2 crossmatch info.
-    """
-
-    if type(source_ids) != np.ndarray:
-        raise TypeError(
-            'source_ids must be np.ndarray of np.int64 Gaia DR2 source_ids'
-        )
-    if type(source_ids[0]) != np.int64:
-        raise TypeError(
-            'source_ids must be np.ndarray of np.int64 Gaia DR2 source_ids'
-        )
-
-    xmltouploadpath = os.path.join(gaiadir,'toupload_{}.xml'.format(groupname))
-    dlpath = os.path.join(gaiadir,'group{}_matches.xml.gz'.format(groupname))
-
-    if overwrite:
-        if os.path.exists(xmltouploadpath):
-            os.remove(xmltouploadpath)
-
-    if not os.path.exists(xmltouploadpath):
-        make_votable_given_source_ids(source_ids, outpath=xmltouploadpath)
-
-    if os.path.exists(dlpath) and overwrite:
-        os.remove(dlpath)
-
-    if not os.path.exists(dlpath):
-
-        Gaia.login(credentials_file=credentials_file)
-
-        jobstr = (
-        '''
-        SELECT *
-        FROM tap_upload.foobar as u, gaiadr2.gaia_source AS g
-        WHERE u.source_id=g.source_id
-        '''
-        )
-        query = jobstr
-
-        # might do async if this times out. but it doesn't.
-        j = Gaia.launch_job(query=query,
-                            upload_resource=xmltouploadpath,
-                            upload_table_name="foobar", verbose=True,
-                            dump_to_file=True, output_file=dlpath)
-
-        Gaia.logout()
-
-    df = given_votable_get_df(dlpath, assert_equal='source_id')
-
-    if len(df) != len(source_ids) and enforce_all_sourceids_viable:
-        errmsg = (
-            'ERROR! got {} matches vs {} source id queries'.
-            format(len(df), len(source_ids))
-        )
-        raise AssertionError(errmsg)
-
-    if len(df) != len(source_ids) and not enforce_all_sourceids_viable:
-        wrnmsg = (
-            'WRN! got {} matches vs {} source id queries'.
-            format(len(df), len(source_ids))
-        )
-        print(wrnmsg)
-
-    return df
-
 
 def query_neighborhood(bounds, groupname, n_max=2000, overwrite=True,
                        is_cg18_group=True, is_kc19_group=False,
