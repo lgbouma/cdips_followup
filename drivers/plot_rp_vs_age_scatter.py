e53d10d20d27698b826bddc911c2a635cd301c1b
lgbouma <bouma.luke@gmail.com>
2019-12-14 14:52:54 -0500

    update rp vs age plot

--- drivers/plot_rp_vs_age_scatter.py
+++ drivers/plot_rp_vs_age_scatter.py
@@ -34,21 +34,13 @@ from astroquery.nasa_exoplanet_archive import NasaExoplanetArchive
 def arr(x):
     return np.array(x)
 
-
-if __name__=='__main__':
-    show_median_error=1
+def plot_rp_vs_age_scatter(active_targets=0):
 
     # columns described at
     # https://exoplanetarchive.ipac.caltech.edu/docs/API_exoplanet_columns.html
-    ea_tab = NasaExoplanetArchive.get_confirmed_planets_table(all_columns=True,
-                                                              show_progress=True)
-
-    # radius in jupiter (earth) radii, period, number of planets in system,
-    # galacitc latitude.
-    wellmeasuredparams = ['pl_radj', 'pl_rade', 'pl_orbper', 'pl_pnum', 'st_glat']
-    # janky measurements where errors matter: age in Gyr, density(g/cc), eccen,
-    # inclination
-    jankyparams = ['pl_dens', 'pl_orbeccen', 'pl_orbincl']
+    ea_tab = NasaExoplanetArchive.get_confirmed_planets_table(
+        all_columns=True, show_progress=True
+    )
 
     # get systems with finite ages (has a value, and +/- error bar)
     has_age_value = ~ea_tab['st_age'].mask
@@ -77,15 +69,15 @@ if __name__=='__main__':
     rp_merr = t['pl_radeerr2']
     rp_errs = np.array([rp_perr, rp_merr]).reshape(2, len(age))
 
-
     # plot age vs all "good cols". (age is on y axis b/c it has the error bars, and
     # I at least skimmed the footnotes of Hogg 2010)
     f,ax = plt.subplots(figsize=(4,3))
 
     # squares
     label = (
-            'NASA exoarchive (median $\sigma_{{\mathrm{{age}}}}$ = {:.1f} Gyr)'.
-            format(np.median(age_errs))
+        'NASA Exoplanet Archive, '+
+        r"$\langle \sigma_{{\mathrm{{age}}}} \rangle$ = "+
+        "{:.1f} Gyr".format(np.median(age_errs))
     )
     ax.scatter(age, rp,
                color='gray', s=3, zorder=1, marker='o', linewidth=0,
@@ -93,7 +85,10 @@ if __name__=='__main__':
 
     # targets
     tdf = pd.read_csv(
-        '/home/luke/Dropbox/proj/cdips_followup/results/20190926_2020A_targets_age_rp.csv'
+        os.path.join(
+            os.path.expanduser("~"),
+            'Dropbox/proj/cdips_followup/results/20190926_2020A_targets_age_rp.csv'
+        )
     )
 
     badids = [
@@ -109,38 +104,57 @@ if __name__=='__main__':
     target_rp = np.array(tdf['rplanet'])
     target_rp_unc = np.array(tdf['rplanet_unc'])
 
-    ax.scatter(
-        target_age, target_rp, color='black', s=25,
-        zorder=3, marker='*', linewidth=0,
-        label='Proposed targets (median $\sigma_{{\mathrm{{age}}}}$ < 0.1 Gyr)'
-    )
+    if active_targets:
+        ax.scatter(
+            target_age, target_rp, color='black', s=25,
+            zorder=3, marker='*', linewidth=0,
+            label=r'Active targets, $\langle \sigma_{{\mathrm{{age}}}} \rangle$ < 0.1 Gyr'
+        )
 
-    ax.errorbar(target_age, target_rp, yerr=target_rp_unc,
-                elinewidth=0.3, ecolor='k', capsize=0, capthick=0, linewidth=0,
-                fmt='*', ms=0, zorder=2, color='black', alpha=0.5)
+        target_rp_rel_unc = target_rp_unc / target_rp
+
+        ##########
+        # HACK: force to 30% relative uncertainty for TOI-520, b/c fits did not
+        # converge
+        sel = target_rp_rel_unc > 0.5
+        target_rp_unc[sel] = target_rp[sel] * 0.3
+        ##########
+
+        ax.errorbar(target_age, target_rp, yerr=target_rp_unc,
+                    elinewidth=0.3, ecolor='k', capsize=0, capthick=0, linewidth=0,
+                    fmt='*', ms=0, zorder=2, color='black', alpha=0.5)
 
     ## error bars
     #ax.errorbar(age, rp, yerr=age_errs, xerr=rp_errs,
     #            elinewidth=0.3, ecolor='k', capsize=0, capthick=0, linewidth=0,
     #            fmt='s', ms=0, zorder=2, alpha=0.05)
 
-    #if show_median_error:
-    #    ax.text(0.03, 0.03, txt, ha='left', va='bottom', fontsize='small',
-    #            zorder=3, transform=ax.transAxes, color='C0')
-
-    ax.legend(loc='best', borderpad=0.1, handletextpad=0.1, fontsize='small',
-              framealpha=1)
+    # flip default legend order
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], loc='lower left', borderpad=0.1,
+              handletextpad=0.1, fontsize='small', framealpha=1)
 
     ax.set_xlabel('Age [Gyr]')
     ax.set_ylabel('Planet radius [R$_\oplus$]')
 
-    #ax.tick_params(top=True, bottom=True, left=True, right=True)
     ax.get_yaxis().set_tick_params(which='both', direction='in')
     ax.get_xaxis().set_tick_params(which='both', direction='in')
 
+    ax.set_ylim([0.13, 85])
+
     ax.set_xscale('log')
     ax.set_yscale('log')
+    ax.tick_params(top=True, bottom=True, left=True, right=True, which='both')
+
 
     f.tight_layout()
-    f.savefig('../results/rp_vs_age_scatter.png', bbox_inches='tight', dpi=300)
-    f.savefig('../results/rp_vs_age_scatter.pdf', bbox_inches='tight')
+    savstr = '_no_overplot' if not active_targets else '_active_targets'
+
+    outpath = '../results/rp_vs_age_scatter{}.png'.format(savstr)
+    f.savefig(outpath, bbox_inches='tight', dpi=450)
+    f.savefig(outpath.replace('.png','.pdf'), bbox_inches='tight')
+    print('made {}'.format(outpath))
+
+if __name__=='__main__':
+    plot_rp_vs_age_scatter(active_targets=0)
+    plot_rp_vs_age_scatter(active_targets=1)
