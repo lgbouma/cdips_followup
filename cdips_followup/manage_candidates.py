288ed4b39f007c6a5aba5a49af5d79d98dd37377
lgbouma <bouma.luke@gmail.com>
2020-01-09 10:56:59 -0500

    manage_candidates: nan handling
    turns out, if you want to be able to write from libreoffice calc, this
    matters.

--- cdips_followup/manage_candidates.py
+++ cdips_followup/manage_candidates.py
@@ -1,30 +1,29 @@
 """
 Manage candidates.csv.
 
-Each row in this CSV file is a planet candidate entry. This database is used to track
-what needs observing, what has been observed, and whether more observations are
-needed.
+Each row in this CSV file is a planet candidate entry. This database is used to
+track what needs observing, what has been and will be observed, and whether
+more observations are needed.
+
+Contents:
+    insert_candidate
+    query_candidate
+    save_candidates_csv_file
 
 Use cases:
 
 * Add candidates given source_id or ticid.
-    * e.g., someone emails with information --> create new entry
-    * include further information from ExoFOP-TESS uploads
-
-* Update columns given new information
-    * Update current_priority
-    * Update pending_spectroscopic_observations
-    * Update pending_photometry_observations
-    * Update comment
-
-* Export for google spreadsheet viewing.
-
-* View spreadsheet to select viable candidates. Viewing can be done in
-Libreoffice Calc, or in Google Spreadsheets. The latter has viewing benefits,
-but cannot be used for updates. (While Libreoffice calc can).
+* Update columns given new information:
+    current_priority
+    pending_spectroscopic_observations
+    pending_photometry_observations
+    comment
+* View spreadsheet to select viable candidates. Viewing is done in Libreoffice
+Calc (separated by "|", with "format quoted field as text"), or else in Google
+Spreadsheets. Google Sheets has nice auto-coloring logic, but cannot be used
+for updates. (While Libreoffice calc can).
 
 `candidates.csv` has columns:
-
     source_id, ticid, toi, targetid, reference, name, nbhd_rating,
     init_priority, current_priority, pending_spectroscopic_observations,
     pending_photometry_observations, comment, gaia_ra, gaia_dec, gaia_plx,
@@ -38,7 +37,7 @@ but cannot be used for updates. (While Libreoffice calc can).
 ######################
 
 import pandas as pd, numpy as np
-import socket, os
+import socket, os, csv
 from datetime import datetime
 from parse import search
 
@@ -89,7 +88,7 @@ def insert_candidate(
         nbhd_rating (0-2, or null),
         init_priority (0-2),
         current_priority (0-2),
-        pending_spectroscopic_observation (str, '' if null),
+        pending_spectroscopic_observations (str, '' if null),
         pending_photometry_observations (str, '' if null),
         comment (str, '' if null)
         candidate_provenance (str, '' if null)
@@ -107,6 +106,8 @@ def insert_candidate(
         source_id = tic_to_gaiadr2(ticid)
 
     toiid = ticid_to_toiid(ticid)
+    if isinstance(toiid, str):
+        toiid = toiid.replace('.01','')
 
     targetid = ticid_and_toiid_to_targetid(ticid, toiid)
 
@@ -116,11 +117,15 @@ def insert_candidate(
 
     cdips_r = get_cdips_pub_catalog_entry(source_id)
 
-    iscdipstarget = True if isinstance(cdips_r, pd.DataFrame) else False
+    iscdipstarget = 1 if isinstance(cdips_r, pd.DataFrame) else 0
 
     if isinstance(cdips_r, pd.DataFrame):
         assert len(cdips_r) == 1
         cdips_r = cdips_r.iloc[0]
+
+        for col in cdips_r.index:
+            if pd.isnull(cdips_r[col]):
+                cdips_r[col] = -1
 
     else:
         cdips_cols = [
@@ -136,7 +141,7 @@ def insert_candidate(
         })
 
         for col in cdips_cols:
-            cdips_r[col] = np.nan
+            cdips_r[col] = '-1'
 
     #
     # Get TIC information, or else assign nans
@@ -158,40 +163,49 @@ def insert_candidate(
             )
             raise AssertionError(errmsg)
 
-    else:
-        tic = [
-            'cluster', 'reference', 'ext_catalog_name', 'ra', 'dec', 'pmra',
-            'pmdec', 'parallax', 'phot_g_mean_mag', 'phot_bp_mean_mag',
-            'phot_rp_mean_mag', 'k13_name_match', 'unique_cluster_name',
-            'how_match', 'not_in_k13', 'comment', 'logt', 'e_logt',
-            'logt_provenance'
-        ]
+        for col in ticcols:
+            if pd.isnull(tic_r[col]):
+                tic_r[col] = -1
 
+    else:
         tic_r = pd.Series({
             'source_id': source_id
         })
 
         for col in ticcols:
-            tic_r[col] = np.nan
+            tic_r[col] = -1
 
     #
-    # Get the fit information as uploaded to ExoFOP-TESS
+    # Get the fit information as uploaded to ExoFOP-TESS. By default, use the
+    # TOI table information. Otherwise, use the CTOI table.
     #
 
     plproperties_r = get_exofop_toi_catalog_entry(ticid)
+    plkeyd = {
+        'rp': 'Planet Radius (R_Earth)',
+        'period': 'Period (days)',
+        'depth': 'Depth (mmag)'
+    }
     if plproperties_r is None:
         plproperties_r = get_exofop_ctoi_catalog_entry(ticid)
+        plkeyd = {
+            'rp': 'Radius (R_Earth)',
+            'period': 'Period (days)',
+            'depth': 'Depth mmag'
+        }
 
     if isinstance(plproperties_r, pd.DataFrame):
         assert len(plproperties_r) == 1
         plproperties_r = plproperties_r.iloc[0]
 
+        for col in plproperties_r.index:
+            if pd.isnull(plproperties_r[col]):
+                plproperties_r[col] = -1
+
     else:
-        plproperties_r = pd.DataFrame({
-            'Planet Radius (R_Earth)': np.nan,
-            'Period (days)': np.nan,
-            'Depth (mmag)': np.nan
-        }, index=[0])
+        plproperties_r = pd.DataFrame(
+            {v:-1 for v in plkeyd.values()}, index=[0]
+        )
 
     #
     # Get: nbhd_rating, init_priority, current_priority,
@@ -232,29 +246,28 @@ def insert_candidate(
             format(source_id)
         )
 
-
     #
     # Construct and insert the new row.
     #
 
     new_row = pd.DataFrame({
-        'source_id': source_id,
-        'ticid': ticid,
-        'toi': toiid,
-        'targetid': targetid,
+        'source_id': str(source_id),
+        'ticid': str(ticid),
+        'toi': str(toiid),
+        'targetid': str(targetid),
         'iscdipstarget': iscdipstarget,
         'reference': cdips_r.reference,
         'name': cdips_r.cluster,
         'age': cdips_r.logt,
-        'nbhd_rating': nbhd_rating,
+        'nbhd_rating': nbhd_rating if not pd.isnull(nbhd_rating) else '--',
         'init_priority': init_priority,
         'current_priority': current_priority,
         'pending_spectroscopic_observations': pending_spectroscopic_observations,
         'pending_photometry_observations': pending_photometry_observations,
         'comment': comment,
-        'rp': plproperties_r['Planet Radius (R_Earth)'],
-        'period': plproperties_r['Period (days)'],
-        'depth': plproperties_r['Depth (mmag)'],
+        'rp': plproperties_r[plkeyd['rp']],
+        'period': plproperties_r[plkeyd['period']],
+        'depth': plproperties_r[plkeyd['depth']],
         'gaia_ra': cdips_r.ra,
         'gaia_dec': cdips_r.dec,
         'gaia_plx': cdips_r.parallax,
@@ -267,7 +280,7 @@ def insert_candidate(
         'tic_Hmag': tic_r.Hmag,
         'tic_Kmag': tic_r.Kmag,
         'tic_Tmag': tic_r.Tmag,
-        'tic_teff': tic_r.Teff,
+        'tic_teff': tic_r.Teff if not pd.isnull(tic_r.Teff) else -1,
         'tic_logg': tic_r.logg,
         'tic_rstar': tic_r.rad,
         'tic_mstar': tic_r.mass,
@@ -277,9 +290,40 @@ def insert_candidate(
         'isretired': isretired
     }, index=[0])
 
-    cand_df = pd.read_csv(CAND_PATH)
+    cand_df = pd.read_csv(CAND_PATH, sep='|')
 
     new_cand_df = pd.concat((cand_df, new_row), sort=False)
+
+    #
+    # convert default non-string columns to strings. ditto for float and
+    # integer columns.
+    #
+    strcols = ['source_id', 'ticid', 'targetid', 'nbhd_rating',
+               'pending_spectroscopic_observations',
+               'pending_photometry_observations', 'comment',
+               'candidate_provenance', 'insert_time', 'last_update_time']
+    for strcol in strcols:
+        new_cand_df[strcol] = new_cand_df[strcol].astype(str)
+
+    floatcols = ['rp', 'period', 'gaia_ra', 'gaia_dec', 'gaia_plx',
+                 'gaia_Gmag', 'gaia_Bmag', 'gaia_Rmag', 'tic_Bmag', 'tic_Vmag',
+                 'tic_Jmag', 'tic_Hmag', 'tic_Kmag', 'tic_Tmag', 'tic_logg',
+                 'tic_rstar', 'tic_mstar']
+    for fcol in floatcols:
+        new_cand_df[fcol] = new_cand_df[fcol].astype(float)
+
+
+    intcols = ['iscdipstarget', 'tic_teff']
+    for icol in intcols:
+        new_cand_df[icol] = new_cand_df[icol].astype(int)
+
+    #
+    # replace empty and "nan" strings with "--" string, which LibreOffice calc
+    # can handle.
+    #
+    new_cand_df = new_cand_df.replace(r'^\s*$', "--", regex=True)
+    new_cand_df = new_cand_df.replace('nan', "--", regex=True)
+    new_cand_df = new_cand_df.replace('None', "--", regex=True)
 
     save_candidates_csv_file(new_cand_df)
 
@@ -304,7 +348,7 @@ def query_candidate(source_id=None, ticid=None):
 
     validate_source_id_ticid(source_id, ticid)
 
-    df = pd.read_csv(CAND_PATH)
+    df = pd.read_csv(CAND_PATH, sep='|')
 
     # Get the newest entry for this source.
     if isinstance(source_id, str):
@@ -347,13 +391,15 @@ def save_candidates_csv_file(cand_df):
 
     if not os.path.exists(CAND_DAY_PATH):
         out_df.to_csv(
-            CAND_DAY_PATH, index=False, float_format='%.2f'
+            CAND_DAY_PATH, index=False, sep='|', quotechar='"',
+            quoting=csv.QUOTE_NONNUMERIC, float_format='%.2f'
         )
         print('{}: backed up to {}'.
               format(datetime.utcnow(), CAND_DAY_PATH))
 
     out_df.to_csv(
-        CAND_PATH, index=False, float_format='%.2f'
+        CAND_PATH, index=False, sep='|', quotechar='"',
+        quoting=csv.QUOTE_NONNUMERIC, float_format='%.2f'
     )
     print('{}: overwrite {} with updated values'.
           format(datetime.utcnow(), CAND_PATH))
