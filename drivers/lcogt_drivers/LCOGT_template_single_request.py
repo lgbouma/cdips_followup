d8f863e86db8ceda7096052e37fb590e80a7c7e0
lgbouma <bouma.luke@gmail.com>
2020-01-15 16:20:54 -0500

    lcogt single request: add maximum n_event option

--- drivers/lcogt_drivers/LCOGT_template_single_request.py
+++ drivers/lcogt_drivers/LCOGT_template_single_request.py
@@ -2,6 +2,7 @@
 Given a source_id, make LCOGT photometry followup requests, and optionally
 submit them to the LCOGT API.
 """
+import numpy as np
 from astropy.time import Time
 from cdips_followup.manage_ephemerides import query_ephemeris
 from cdips_followup.LCOGT_dedicated_requests import (
@@ -14,28 +15,28 @@ def main():
     ####################
     overwrite = 1
     validate = 1
-    submit = 0
+    submit = 1
+    savstr = '20200115_2m_requests' # eg, 20191207_TOI1098_request_2m_tc_secondary. "ephemupdate" if it is one. (this cancels pending observations)
 
-    savstr = '20200115_1m_requests' # eg, 20191207_TOI1098_request_2m_tc_secondary. "ephemupdate" if it is one. (this cancels pending observations)
-    source_id = '5240531632175135616'
+    source_id = '5256502966294604800'
+    max_n_events = 6 # else None. n_events is per eventclass.
 
     filtermode = 'ip'
-    telescope_class = '1m0'
+    telescope_class = '2m0'
     max_duration_error = 20
     max_search_time = Time('2020-04-15 23:59:00')
     manual_ephemeris = False
 
     create_eventclasses = ['OIBEO']
     # create_eventclasses = ['OIBEO', 'IBEO', 'OIBE', 'OIB', 'BEO']
-    # create_eventclasses = ['OIBEO', 'IBEO', 'OIBE']
+    # create_eventclasses = ['OIBEO', 'OIBE', 'IBEO']
 
     submit_eventclasses = ['OIBEO']
     # submit_eventclasses = ['OIBEO', 'IBEO', 'OIBE', 'OIB', 'BEO']
-    # submit_eventclasses = ['OIBEO', 'IBEO', 'OIBE']
+    # submit_eventclasses = ['OIBEO', 'OIBE', 'IBEO']
     # submit_eventclasses = ['OIB', 'BEO']
 
     ####################
-
 
     if manual_ephemeris:
         period = 42
@@ -56,9 +57,48 @@ def main():
                                      filtermode=filtermode,
                                      telescope_class=telescope_class)
 
+    # if a maximum number of events is set, impose it!
+    if isinstance(max_n_events, int):
+
+        _requests = []
+        for ix in range(len(create_eventclasses)):
+            print('starting with {} {} events.'.
+                  format(len(requests[ix]), create_eventclasses[ix])
+            )
+
+        for eventclass in requests:
+            _eventclass = []
+            starttimes = []
+            for req in eventclass:
+                starttimes.append(req['requests'][0]['windows'][0]['start'])
+
+            # sort by start time, cut to get the closest ones.
+            sort_times = np.sort(starttimes)
+            sel_times = sort_times[ : max_n_events]
+
+            for req in eventclass:
+                starttime = req['requests'][0]['windows'][0]['start']
+                if starttime in sel_times:
+                    _eventclass.append(req)
+
+            if len(_eventclass) > 0:
+                _requests.append(_eventclass)
+
+        assert len(_requests[0]) <= max_n_events
+        requests = _requests
+
+        print('WRN!: trimmed to {} events.'.format(len(requests[0])))
+        if len(sel_times)>0:
+            print('WRN!: max time: \n{}'.format(repr(sel_times[-1])))
+            print('\nWRN!: selected times: \n{}'.format(repr(sel_times)))
+        else:
+            print('WRN!: got no times')
+
+
     given_dedicated_requests_validate_submit(
         requests, submit_eventclasses, validate=validate, submit=submit,
         max_duration_error=max_duration_error)
+
 
 if __name__ == "__main__":
     main()
