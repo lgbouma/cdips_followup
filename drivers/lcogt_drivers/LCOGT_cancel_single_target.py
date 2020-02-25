7a7b577203f9d1627b5994cb1991aa94db1ffa8e
lgbouma <bouma.luke@gmail.com>
2020-02-25 11:51:49 -0500

    cand db update

--- drivers/lcogt_drivers/LCOGT_cancel_single_target.py
+++ drivers/lcogt_drivers/LCOGT_cancel_single_target.py
@@ -2,6 +2,6 @@ from cdips_followup.LCOGT_update_requests import cancel_pending_requests
 
 # targetid = '580.01'
 # targetid = 'TIC133260600.01'
-targetid = 'TIC133260600.01'
+targetid = 'TIC46029346.01'
 
 cancel_pending_requests(targetid)
