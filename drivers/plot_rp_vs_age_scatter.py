2f52c192cbd34b292b107636b3154f8b363ce977
lgbouma <bouma.luke@gmail.com>
2020-02-25 11:51:37 -0500

    rp vs age: add HJs only option
    ... for public talk

--- drivers/plot_rp_vs_age_scatter.py
+++ drivers/plot_rp_vs_age_scatter.py
@@ -27,10 +27,12 @@ import os
 
 from astroquery.nasa_exoplanet_archive import NasaExoplanetArchive
 
+from cdips.utils import today_YYYYMMDD
+
 def arr(x):
     return np.array(x)
 
-def plot_rp_vs_age_scatter(active_targets=0, split_toi_ctoi=0):
+def plot_rp_vs_age_scatter(active_targets=0, split_toi_ctoi=0, hjs_only=0):
 
     #
     # columns described at
@@ -56,6 +58,10 @@ def plot_rp_vs_age_scatter(active_targets=0, split_toi_ctoi=0):
 
     t = ea_tab[sel]
 
+    if hjs_only:
+        is_hj = (t['pl_orbper'] < 10*u.day) & (t['pl_rade'] > 7)
+        t = t[is_hj]
+
     #
     # read params
     #
@@ -80,6 +86,10 @@ def plot_rp_vs_age_scatter(active_targets=0, split_toi_ctoi=0):
         r"$\langle \sigma_{{\mathrm{{age}}}} \rangle$ = "+
         "{:.1f} Gyr".format(np.median(age_errs))
     )
+    if hjs_only:
+        label = (
+            'Known hot Jupiters'
+        )
     ax.scatter(age, rp,
                color='gray', s=3, zorder=1, marker='o', linewidth=0,
                label=label, alpha=1)
@@ -166,6 +176,10 @@ def plot_rp_vs_age_scatter(active_targets=0, split_toi_ctoi=0):
             r'Active targets, $\langle \sigma_{{\mathrm{{age}}}} \rangle$'
             '< 0.1 Gyr'
         )
+        if hjs_only:
+            label = (
+                'Candidate hot Jupiters'
+            )
 
         istoi = ~(sdf.toi == '--')
 
@@ -176,6 +190,12 @@ def plot_rp_vs_age_scatter(active_targets=0, split_toi_ctoi=0):
             )
             ax.scatter(
                 target_age[~istoi], target_rp[~istoi], color='black', s=25,
+                zorder=3, marker='*', linewidth=0, label=label
+            )
+        elif hjs_only:
+            hj = (target_rp > 7) & (target_rp < 29)
+            ax.scatter(
+                target_age[hj], target_rp[hj], color='black', s=25,
                 zorder=3, marker='*', linewidth=0, label=label
             )
         else:
@@ -195,45 +215,81 @@ def plot_rp_vs_age_scatter(active_targets=0, split_toi_ctoi=0):
         target_rp_rel_unc = target_rp_unc / target_rp
 
         ##########
-        # HACK: force to 30% relative uncertainty for TOI-520, b/c fits did not
-        # converge
-        sel = target_rp_rel_unc > 0.5
-        target_rp_unc[sel] = target_rp[sel] * 0.3
+        if hjs_only:
+            # HACK: force to 10% relative uncertainty (eep)
+            sel = target_rp_rel_unc > 0.1
+            target_rp_unc[sel] = target_rp[sel] * 0.1 * (
+                np.random.uniform(0.8,1.2,
+                                  size=len(target_rp[sel]))
+            )
         ##########
 
-        ax.errorbar(target_age, target_rp, yerr=target_rp_unc, elinewidth=0.3,
-                    ecolor='k', capsize=0, capthick=0, linewidth=0, fmt='*',
-                    ms=0, zorder=2, color='black', alpha=0.5)
+        if hjs_only:
+            ax.errorbar(target_age[hj], target_rp[hj], yerr=target_rp_unc[hj],
+                        elinewidth=0.3, ecolor='k', capsize=0, capthick=0,
+                        linewidth=0, fmt='*', ms=0, zorder=2, color='black',
+                        alpha=0.5)
+        else:
+            ax.errorbar(target_age, target_rp, yerr=target_rp_unc,
+                        elinewidth=0.3, ecolor='k', capsize=0, capthick=0,
+                        linewidth=0, fmt='*', ms=0, zorder=2, color='black',
+                        alpha=0.5)
 
     # flip default legend order
     handles, labels = ax.get_legend_handles_labels()
-    ax.legend(handles[::-1], labels[::-1], loc='lower left', borderpad=0.1,
-              handletextpad=0.1, fontsize='small', framealpha=1)
+    if not hjs_only:
+        leg = ax.legend(handles[::-1], labels[::-1], loc='lower left',
+                        borderpad=0.3, handletextpad=0.3, fontsize='small',
+                        framealpha=1)
+    else:
+        leg = ax.legend(handles[::-1], labels[::-1], loc='upper right',
+                        borderpad=0.3, handletextpad=0.3, fontsize='small',
+                        framealpha=1)
 
-    ax.set_xlabel('Age [Gyr]')
-    ax.set_ylabel('Planet radius [R$_\oplus$]')
+    leg.get_frame().set_linewidth(0.5)
 
+    ax.set_xlabel('Age [billion years]')
+    ax.set_ylabel('Planet size [Earth radii]')
+
+    ax.set_xlim([8e-3, 17])
+    ax.set_xticklabels([0.01, 0.1, 1, 10])
     ax.get_yaxis().set_tick_params(which='both', direction='in')
     ax.get_xaxis().set_tick_params(which='both', direction='in')
 
-    ax.set_ylim([0.13, 85])
+    if not hjs_only:
+        ax.set_ylim([0.13, 85])
+        ax.set_yscale('log')
+    if hjs_only:
+        ax.set_ylim([4, 36])
+        ax.set_yscale('linear')
 
     ax.set_xscale('log')
-    ax.set_yscale('log')
     ax.tick_params(top=True, bottom=True, left=True, right=True, which='both')
 
     f.tight_layout()
     savstr = '_no_overplot' if not active_targets else '_active_targets'
     if split_toi_ctoi:
         savstr += '_split_ctoi'
+    if hjs_only:
+        savstr += '_onlyhjs'
 
-    outpath = '../results/rp_vs_age_scatter/rp_vs_age_scatter{}.png'.format(savstr)
+    outpath = (
+        '../results/rp_vs_age_scatter/rp_vs_age_scatter_{}{}.png'.
+        format(today_YYYYMMDD(), savstr)
+    )
     f.savefig(outpath, bbox_inches='tight', dpi=450)
     f.savefig(outpath.replace('.png','.pdf'), bbox_inches='tight')
     print('made {}'.format(outpath))
 
+
 if __name__=='__main__':
+
+    plot_rp_vs_age_scatter(active_targets=0, hjs_only=1)
+    plot_rp_vs_age_scatter(active_targets=1, split_toi_ctoi=0, hjs_only=1)
+    assert 0
 
     plot_rp_vs_age_scatter(active_targets=0)
     plot_rp_vs_age_scatter(active_targets=1, split_toi_ctoi=0)
     plot_rp_vs_age_scatter(active_targets=1, split_toi_ctoi=1)
+
+
