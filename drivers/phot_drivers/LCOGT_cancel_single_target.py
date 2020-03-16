a57164bcb6f8400558b4f8cdeda60e9617e6b122
lgbouma <bouma.luke@gmail.com>
2020-03-15 20:36:24 -0400

    minor

--- drivers/phot_drivers/LCOGT_cancel_single_target.py
+++ drivers/phot_drivers/LCOGT_cancel_single_target.py
@@ -1,7 +1,6 @@
 from cdips_followup.LCOGT_update_requests import cancel_pending_requests
 
 # targetid = '580.01'
-# targetid = 'TIC133260600.01'
-targetid = 'TIC269861070.01'
+targetid = 'TIC59859387.01'
 
 cancel_pending_requests(targetid)
