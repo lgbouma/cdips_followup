6a236766104ad5bc1d9d5a7da686ea9f39750b49
lgbouma <bouma.luke@gmail.com>
2020-03-31 19:37:14 -0400

    ditto

--- drivers/update_candidate_db_rotation_params.py
+++ drivers/update_candidate_db_rotation_params.py
@@ -43,27 +43,63 @@ def main():
         #
         # this list was a one-off manual update from SP0 targets on 2020/03/24
         #
-        ticids = ['268301217', '257605131', '460205581', '360630575',
-                  '59859387', '383390264', '166527623', '62483237',
-                  '217933560', '198262850']
-        rot_qualitys = ['0', '0', '0', '0',
-                        '1', '0', '0', '0',
-                        '0', '0']
-        Prots = [7*u.day, 4.5*u.day, None, 6.5*u.day,
-                 None, 1.8*u.day, 1.5*u.day, 8*u.day,
-                 3*u.day, 2.2*u.day]
-        vsinis = [8*u.km/u.s, None, 17*u.km/u.s, None,
-                  27.2*u.km/u.s, None, None, None,
-                  None, None]
-        rot_amps = [1.50E-02, 2.00E-02, 2.00E-02, 1.50E-02,
-                    1.00E-03, 5.00E-03, 2.00E-02, 4.00E-03,
-                    8.00E-02, 4.10E-02]
-        Mp_preds = [1*u.Mjup, 15.6*u.Mearth, 49.6*u.Mearth, 6*u.Mearth,
-                    20*u.Mearth, 9.5*u.Mearth, 20*u.Mearth, 10*u.Mearth,
-                    1*u.Mjup, 15*u.Mearth]
-        Tdurs = [1.38*u.hour, 4.436*u.hour, 1.775*u.hour, 2.343*u.hour,
-                 1.73*u.hour, 2.64*u.hour, 3.1*u.hour, 2.136*u.hour,
-                 2.54*u.hour, 1.92*u.hour ]
+        ticids = [
+            '180987952', '130415266', '154671430',
+            '462162963', '140978827', '238597883',
+            '117789567', '67600790'
+        ]
+        rot_qualitys = [
+            '0', '1', '0',
+            '0', '0', '1',
+            '1', '0'
+        ]
+        Prots = [
+            0.5*u.day, 0.5*u.day, 5*u.day,
+            1*u.day, None, None,
+            None, 1*u.day
+        ]
+        vsinis = [
+            200*u.km/u.s, 200*u.km/u.s, 12*u.km/u.s,
+            50*u.km/u.s, 13*u.km/u.s, 100*u.km/u.s,
+            200*u.km/u.s, None
+        ]
+        rot_amps = [
+            0.001, 0.001, 0.001,
+            0.001, 0.002, 0.001,
+            0.001, 0.05
+        ]
+        Mp_preds = [
+            20*u.Mearth, 317.83*u.Mearth, 17*u.Mearth,
+            317.83*u.Mearth, 317.83*u.Mearth, 20*u.Mearth,
+            317.83*u.Mearth, 30*u.Mearth
+        ]
+        Tdurs = [
+            1.514*u.hour, 6.1*u.hour, 2*u.hour,
+            1.67*u.hour, 2.03*u.hour, 2.57*u.hour,
+            1.33*u.hour, 1.22*u.hour
+        ]
+
+        # ticids = ['268301217', '257605131', '460205581', '360630575',
+        #           '59859387', '383390264', '166527623', '62483237',
+        #           '217933560', '198262850']
+        # rot_qualitys = ['0', '0', '0', '0',
+        #                 '1', '0', '0', '0',
+        #                 '0', '0']
+        # Prots = [7*u.day, 4.5*u.day, None, 6.5*u.day,
+        #          None, 1.8*u.day, 1.5*u.day, 8*u.day,
+        #          3*u.day, 2.2*u.day]
+        # vsinis = [8*u.km/u.s, None, 17*u.km/u.s, None,
+        #           27.2*u.km/u.s, None, None, None,
+        #           None, None]
+        # rot_amps = [1.50E-02, 2.00E-02, 2.00E-02, 1.50E-02,
+        #             1.00E-03, 5.00E-03, 2.00E-02, 4.00E-03,
+        #             8.00E-02, 4.10E-02]
+        # Mp_preds = [1*u.Mjup, 15.6*u.Mearth, 49.6*u.Mearth, 6*u.Mearth,
+        #             20*u.Mearth, 9.5*u.Mearth, 20*u.Mearth, 10*u.Mearth,
+        #             1*u.Mjup, 15*u.Mearth]
+        # Tdurs = [1.38*u.hour, 4.436*u.hour, 1.775*u.hour, 2.343*u.hour,
+        #          1.73*u.hour, 2.64*u.hour, 3.1*u.hour, 2.136*u.hour,
+        #          2.54*u.hour, 1.92*u.hour ]
 
         for ticid, rot_quality, Prot, vsini, rot_amp, Mp_pred, Tdur in zip(
             ticids, rot_qualitys, Prots, vsinis, rot_amps, Mp_preds, Tdurs
