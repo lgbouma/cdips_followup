d3d5186dcf0cda2a41252c3032cdcd1058c58ee2
lgbouma <bouma.luke@gmail.com>
2022-05-12 13:42:34 -0700

    paths: add PHOTDIR

--- cdips_followup/paths.py
+++ cdips_followup/paths.py
@@ -1,4 +1,5 @@
 import os
 from cdips_followup import __path__
 DATADIR = os.path.join(os.path.dirname(__path__[0]), 'data')
+PHOTDIR = os.path.join(os.path.dirname(__path__[0]), 'data', 'phot')
 RESULTSDIR = os.path.join(os.path.dirname(__path__[0]), 'results')
