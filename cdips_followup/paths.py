c7965544525d1522f18b0f16631054b196cbb94b
lgbouma <bouma.luke@gmail.com>
2022-05-12 15:08:27 -0700

    paths: add localdir

--- cdips_followup/paths.py
+++ cdips_followup/paths.py
@@ -3,3 +3,11 @@ from cdips_followup import __path__
 DATADIR = os.path.join(os.path.dirname(__path__[0]), 'data')
 PHOTDIR = os.path.join(os.path.dirname(__path__[0]), 'data', 'phot')
 RESULTSDIR = os.path.join(os.path.dirname(__path__[0]), 'results')
+
+LOCALBASEDIR = os.path.join(os.path.expanduser('~'), 'local')
+if not os.path.exists(LOCALBASEDIR):
+    os.mkdir(LOCALBASEDIR)
+
+LOCALDIR = os.path.join(os.path.expanduser('~'), 'local', 'cdips_followup')
+if not os.path.exists(LOCALDIR):
+    os.mkdir(LOCALDIR)
