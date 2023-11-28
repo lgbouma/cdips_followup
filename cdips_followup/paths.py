fbb3018b242850df8d8accce0a2033c7667341b7
lgbouma <bouma.luke@gmail.com>
2023-11-28 09:53:00 -0800

    add unpopular cache dir

--- cdips_followup/paths.py
+++ cdips_followup/paths.py
@@ -6,9 +6,10 @@ SPECDIR = os.path.join(os.path.dirname(__path__[0]), 'data', 'spectra')
 RESULTSDIR = os.path.join(os.path.dirname(__path__[0]), 'results')
 
 LOCALBASEDIR = os.path.join(os.path.expanduser('~'), 'local')
-if not os.path.exists(LOCALBASEDIR):
-    os.mkdir(LOCALBASEDIR)
+if not os.path.exists(LOCALBASEDIR): os.mkdir(LOCALBASEDIR)
 
 LOCALDIR = os.path.join(os.path.expanduser('~'), 'local', 'cdips_followup')
-if not os.path.exists(LOCALDIR):
-    os.mkdir(LOCALDIR)
+if not os.path.exists(LOCALDIR): os.mkdir(LOCALDIR)
+
+FFICACHEDIR = os.path.join(os.path.expanduser('~'), 'local', 'unpopular_ffi_cache')
+if not os.path.exists(FFICACHEDIR): os.mkdir(FFICACHEDIR)
